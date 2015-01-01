0e1f2def3d06acafed7f2bd348ae62ddbaf7fe1c
Daniel Kehoe <daniel@danielkehoe.com>
2015-01-01 17:19:49 +0200

    Rails 4.2

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,8 @@ module RailsFoundation
     # For Foundation 5
     config.assets.precompile += %w( vendor/modernizr )
 
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
